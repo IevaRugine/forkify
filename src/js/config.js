a56e71e2f79def738901b7297643ce8d57385ac3
IevaRugine <ieva.rugine@gmail.com>
2021-09-17 14:23:22 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2e87f003-bce5-4d97-bc5d-50cc9baa9480';
+export const MODAL_CLOSE_SEC = 2.5;
